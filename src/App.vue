f6546be80f6415e4e58708dd9c2677c9f5248c5c
Christopher Jennings <chrisj@kentico.com>
2018-02-09 10:15:08 -0500

    Extract footer as component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
 <template>
   <div id="app">
-    <NavMain />
+    <nav-main />
     <router-view :key="$route.fullPath" />
-    <footer class="footer">
-      <div class="container">
-        <div class="content has-text-centered">
-          <p>
-            Powered by <strong><a href="https://kenticocloud.com/">Kentico Cloud</a></strong>.
-          </p>
-        </div>
-      </div>
-    </footer>
+    <nav-footer />
   </div>
 </template>
 
 <script>
 import NavMain from '@/components/nav-main'
+import NavFooter from '@/components/nav-footer'
 
 export default {
   components: {
-    NavMain
+    NavMain,
+    NavFooter
   }
 }
 </script>
---
+++ src/components/nav-footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+          <p>
+            Powered by <strong><a href="https://kenticocloud.com/">Kentico Cloud</a></strong>.
+          </p>
+        </div>
+      </div>
+    </footer>
+</template>
