dff96c4c2752737f74de950b97e6533e597dfefa
Christopher Jennings <chrisj@kentico.com>
2018-03-21 14:54:32 -0400

    Add container to Hero component

--- src/components/app-hero.vue
+++ src/components/app-hero.vue
@@ -1,16 +1,18 @@
 <template>
   <section class="hero is-primary">
       <div class="hero-body">
-        <div class="columns is-vcentered">
-          <div class="column">
-            <p class="title">{{ title }}</p>
-            <p class="subtitle">{{ subtitle }}</p>
-            <p v-if="text">{{ text }}</p>
-          </div>
-          <div class="column is-one-fifth" v-if="imageUrl">
-            <figure class="image is-1by1 is-cropped">
-              <img :src="imageUrl">
-            </figure>
+        <div class="container">
+          <div class="columns is-vcentered">
+            <div class="column">
+              <p class="title">{{ title }}</p>
+              <p class="subtitle">{{ subtitle }}</p>
+              <p v-if="text">{{ text }}</p>
+            </div>
+            <div class="column is-one-fifth" v-if="imageUrl">
+              <figure class="image is-1by1 is-cropped">
+                <img :src="imageUrl">
+              </figure>
+            </div>
           </div>
         </div>
       </div>
